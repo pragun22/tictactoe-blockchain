4ddfbed13f4ca7b15e949a2157989c5e78446d22
Akshay Kharbanda <akshaykharbanda.ak99@gmail.com>
2019-09-14 23:12:48 +0530

    test8 updated and test9

--- test/test9.js
+++ test/test9.js
@@ -7,21 +7,18 @@ contract("tictactoe", accounts =>{
     const check1 = "cell already filled";
     const check2 = "player 1 made move";
     const check3 = "player 2 made move";
-    // const st3 =  "Waiting for player 2";
-    // const st4 = "Game is currently going on";
-    // const st5 = "Hacker trying to enter as player 1";
-    // const st6 = "Hacker trying to enter as player 2";
 
-    it("player2 trying to move in occupied cell", async()=> {
+    it("player1 trying to move in occupied cell", async()=> {
     
         let tic = await tictactoe.deployed();
         await tic.joinplayer1({from:p1});
         await tic.joinplayer2({from:p2});  
         await tic.game(5,{from:p1});
         let st1 = await tic.gameStatus();
-        
-        await tic.game(5,{from:p2});
+        await tic.game(7,{from:p2});
         let st2 = await tic.gameStatus();
+        await tic.game(7,{from:p1});
+        let st3 = await tic.gameStatus();
 
         assert.equal(
             st1,
@@ -30,8 +27,13 @@ contract("tictactoe", accounts =>{
         );
         assert.equal(
             st2,
+            check3,
+            "player 2 moved at 7"
+        );
+        assert.equal(
+            st3,
             check1,
-            "player 2 trying to move at occupied cell 5"
+            "player 1 trying to move at occupied cell 7"
         );
     });
 
